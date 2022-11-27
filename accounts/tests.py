f299758a36a006b93a867050297ec667b7c1b7a3
gitskeo <skeosouvanh@gmail.com>
2022-11-27 17:34:54 -0600

    added tests and updated website views for comments to make things look cleaner

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import reverse
 # Create your tests here.
+
+
+class SignupageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class HomepageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutpageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/articles/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+
+class NewpageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/articles/new/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("article_new"))
+        self.assertEqual(response.status_code, 302)
+
+
+class SearchpageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/articles/search/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("search"))
+        self.assertEqual(response.status_code, 200)
